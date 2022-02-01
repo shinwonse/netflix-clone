396a3abf285c4065a498e430b9e07559760123fd
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-02-01 22:31:20 +0900

    feat: :lipstick: update home screen

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+const API_KEY = "2ba38dbd419e938d0ac1a4b4f4219034";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMoive {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMoive[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
